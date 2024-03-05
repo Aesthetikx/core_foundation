ade4ff9068fccc7c9a2ac9d5660f8488a2c47ad8
John DeSilva <desilvjo@umich.edu>
2024-03-04 19:06:58 -0500

    Extend FFI::Library

--- lib/core_foundation.rb
+++ lib/core_foundation.rb
@@ -2,7 +2,13 @@
 
 require_relative "core_foundation/version"
 
+require "ffi"
+
 # This module is a wrapper for a subset of the CoreFoundation framework.
 module CoreFoundation
   class Error < StandardError; end
+
+  extend FFI::Library
+
+  ffi_lib "/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation"
 end
