75e47b14e245359202cf3224277d26d883f2eec2
John DeSilva <desilvjo@umich.edu>
2024-03-04 19:09:47 -0500

    Implement UUID functions

--- lib/core_foundation.rb
+++ lib/core_foundation.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "core_foundation/version"
-
 require "ffi"
 
 # This module is a wrapper for a subset of the CoreFoundation framework.
@@ -12,3 +10,6 @@ module CoreFoundation
 
   ffi_lib "/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation"
 end
+
+require_relative "core_foundation/version"
+require_relative "core_foundation/uuid"
---
+++ lib/core_foundation/uuid.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module CoreFoundation
+  class CFUUIDBytes < FFI::Struct
+    layout :byte0, :uchar,
+           :byte1, :uchar,
+           :byte2, :uchar,
+           :byte3, :uchar,
+           :byte4, :uchar,
+           :byte5, :uchar,
+           :byte6, :uchar,
+           :byte7, :uchar,
+           :byte8, :uchar,
+           :byte9, :uchar,
+           :byte10, :uchar,
+           :byte11, :uchar,
+           :byte12, :uchar,
+           :byte13, :uchar,
+           :byte14, :uchar,
+           :byte15, :uchar
+  end
+
+  typedef :ulong,   :cf_type_id
+  typedef :pointer, :cf_allocator_ref
+  typedef :pointer, :cf_uuid_ref
+  typedef :pointer, :cf_string_ref
+
+  # rubocop:disable Style/SymbolArray
+  attach_function :CFUUIDGetTypeID,                [],                                        :cf_type_id
+  attach_function :CFUUIDCreate,                   [:cf_allocator_ref],                       :cf_uuid_ref
+  attach_function :CFUUIDCreateWithBytes,          [:cf_allocator_ref, *[:uchar] * 16],       :cf_uuid_ref
+  attach_function :CFUUIDCreateFromString,         [:cf_allocator_ref, :cf_string_ref],       :cf_uuid_ref
+  attach_function :CFUUIDCreateString,             [:cf_allocator_ref, :cf_uuid_ref],         :cf_string_ref
+  attach_function :CFUUIDGetConstantUUIDWithBytes, [:cf_allocator_ref, *[:uchar] * 16],       :cf_uuid_ref
+  attach_function :CFUUIDGetUUIDBytes,             [:cf_uuid_ref],                            CFUUIDBytes.by_value
+  attach_function :CFUUIDCreateFromUUIDBytes,      [:cf_allocator_ref, CFUUIDBytes.by_value], :cf_uuid_ref
+  # rubocop:enable Style/SymbolArray
+end
